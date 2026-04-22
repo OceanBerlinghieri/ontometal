e321e8d398c66af387c4f91a1a3cc7f7542580f9
Ocean Berlinghieri <64857257+OceanBerlinghieri@users.noreply.github.com>
2026-04-22 08:56:55 +0200

    CSV Resource Impl (#11)

--- src/etl/pipeline.py
+++ src/etl/pipeline.py
@@ -1,3 +1,14 @@
+
+import os
+from etl.repository.resource.csv_resource import CSVResource
+
 class Pipeline():
+
     def run(self):
-        print("Running pipeline...")
+        working_dir = os.getcwd()
+        csv_resource = CSVResource()
+
+        band_path = os.path.join(working_dir, 'src', 'etl', 'raw', 'metal_bands.csv')
+        
+        df = csv_resource.load(file_path=band_path, header=1)
+        print(df.head())
