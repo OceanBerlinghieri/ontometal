2577f7832cf63ee00abf7526b54f72413581c6d5
Ocean Berlinghieri <64857257+OceanBerlinghieri@users.noreply.github.com>
2026-04-22 08:56:55 +0200

    CSV Resource Impl (#11)

--- src/etl/main.py
+++ src/etl/main.py
@@ -1,4 +1,4 @@
-from pipeline import Pipeline
+from etl.pipeline import Pipeline
 
 if __name__ == "__main__":
     pipeline = Pipeline()
---
+++ src/etl/repository/resource/base_resource.py
@@ -0,0 +1,11 @@
+from abc import ABC, abstractmethod
+import pandas as pd
+
+class BaseResource(ABC):
+    @abstractmethod
+    def load(self, file_path: str, header: int = 0) -> pd.DataFrame:
+        pass
+    
+    @abstractmethod
+    def save(self, data: pd.DataFrame, file_path: str):
+        pass
