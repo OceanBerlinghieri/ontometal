976360e4b9f02d56654e565e4520bb8e150522f2
Ocean Berlinghieri <berlinghieri10@gmail.com>
2026-04-22 08:52:14 +0200

    Added csv_resource functions

---
+++ setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+import os
+
+setup(
+    name="ontometal",
+    version="0.0.0-dev",
+    description="Ontometal ETL pipeline",
+    author="Ocean Berlinghieri",
+    author_email="berlinghieri10@gmail.com",
+    url="https://github.com/oceanberlinghieri/ontometal",
+    license="Apache 2.0",
+    
+    # Package discovery
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    
+    # Include non-Python files
+    package_data={
+        "": ["*.ttl", "*.rdfs", "*.csv", "*.xml"],
+    },
+    include_package_data=True,
+    
+    # Python version requirement
+    python_requires=">=3.10",
+    
+    entry_points={
+        "console_scripts": [
+            "ontometal=etl.main:main",
+        ],
+    },
+
+    # Dependencies
+    install_requires=[
+        "pandas>=3.0.2",
+    ],
+    
+    # Optional dependencies
+    extras_require={
+        "dev": [
+            "pytest>=6.0.0",
+            "black>=22.0.0",
+            "flake8>=4.0.0",
+        ],
+    },
+)
