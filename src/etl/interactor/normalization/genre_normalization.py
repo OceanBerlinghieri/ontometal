4d8cba5262a614af6fcbb0031422b481d38a525e
Ocean Berlinghieri <berlinghieri10@gmail.com>
2026-04-23 07:48:22 +0200

    Get genres from bands and labels

---
+++ src/etl/interactor/normalization/genre_normalization.py
@@ -0,0 +1,11 @@
+from pandas import DataFrame
+import pandas as pd
+
+
+class GenreNormalization:
+    def __init__(self):
+        pass
+
+    def normalize(self, genres: DataFrame) -> DataFrame:
+        # Implementation for genre normalization
+        return pd.DataFrame()
--- src/etl/pipeline.py
+++ src/etl/pipeline.py
@@ -1,15 +1,42 @@
 import os
-from etl.repository.resource.csv_resource import CSVResource
+
+import pandas as pd
+
+from etl.repository.band_repository import BandRepository
+from etl.repository.country_repository import CountryRepository
+from etl.repository.genre_repository import GenreRepository
+from etl.repository.label_repository import LabelRepository
+from etl.repository.release_repository import ReleaseRepository
 
 
 class Pipeline:
-    def __init__(self, band_repository):
+    def __init__(
+        self,
+        genre_repository: GenreRepository,
+        country_repository: CountryRepository,
+        release_repository: ReleaseRepository,
+        label_repository: LabelRepository,
+        band_repository: BandRepository,
+    ):
+        self.genre_repository = genre_repository
+        self.country_repository = country_repository
+        self.release_repository = release_repository
+        self.label_repository = label_repository
         self.band_repository = band_repository
 
     def run(self):
         working_dir = os.getcwd()
-        bands = self.band_repository.get_bands(
+        band_genres = self.genre_repository.get_band_genres(
             path=os.path.join(working_dir, "src/etl/raw", "metal_bands.csv")
         )
 
-        print(bands.head())
+        label_specializations = self.genre_repository.get_label_specializations(
+            path=os.path.join(working_dir, "src/etl/raw", "labels_roster.csv")
+        )
+
+        all_genres = (
+            pd.concat([band_genres, label_specializations]).drop_duplicates().dropna()
+        )
+
+        print("All genres:")
+        print(all_genres)
