97b9ef16aff3e09842bb2533ce90b9891184ed63
Ocean Berlinghieri <berlinghieri10@gmail.com>
2026-04-23 06:51:45 +0200

    Band repository implemented

--- src/etl/pipeline.py
+++ src/etl/pipeline.py
@@ -1,14 +1,12 @@
-
 import os
 from etl.repository.resource.csv_resource import CSVResource
 
 class Pipeline():
+    def __init__(self, band_repository):
+        self.band_repository = band_repository
 
     def run(self):
         working_dir = os.getcwd()
-        csv_resource = CSVResource()
+        bands = self.band_repository.get_bands(path=os.path.join(working_dir, "src/etl/raw", "metal_bands.csv"))
 
-        band_path = os.path.join(working_dir, 'src', 'etl', 'raw', 'metal_bands.csv')
-        
-        df = csv_resource.load(file_path=band_path, header=1)
-        print(df.head())
+        print(bands.head())
---
+++ src/etl/repository/band_repository_impl.py
@@ -0,0 +1,10 @@
+from etl.repository.band_repository import BandRepository
+from etl.repository.resource.base_resource import BaseResource
+from pandas import DataFrame
+
+class BandRepositoryImpl(BandRepository):
+    def __init__(self, resource: BaseResource):
+        self.resource = resource
+
+    def get_bands(self, path, header=0) -> DataFrame:
+        return self.resource.load(file_path=path, header=header)
