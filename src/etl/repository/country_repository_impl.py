b7d40a92cc7b29ed794258c9f063129a07f6fd1c
Ocean Berlinghieri <berlinghieri10@gmail.com>
2026-04-24 07:15:13 +0200

    Country normalization

--- src/etl/repository/country_repository_impl.py
+++ src/etl/repository/country_repository_impl.py
@@ -10,3 +10,9 @@ class CountryRepositoryImpl(CountryRepository):
 
     def get_countries(self, path, header=0) -> DataFrame:
         return self.resource.load(file_path=path, header=header)
+
+    def get_band_countries(self, path, header=0) -> DataFrame:
+        return self.resource.load(file_path=path, header=header)[["Country"]]
+
+    def get_label_countries(self, path, header=0) -> DataFrame:
+        return self.resource.load(file_path=path, header=header)[["Country"]]
