8915178804ac3f04da6e7229c1df7d5a4cd4c32f
Ocean Berlinghieri <berlinghieri10@gmail.com>
2026-04-28 08:15:12 +0200

    Previous metadata remove

--- src/rdf_conversion/interactor/metadata_adder.py
+++ src/rdf_conversion/interactor/metadata_adder.py
@@ -6,18 +6,22 @@ from rdf_conversion import ONTOMETAL_VERSION, ONTOMETAL_VERSION_INFO
 class MetadataAdder:
     def add_metadata(self, graph: Graph) -> Graph:
         ontology_uri = URIRef("https://w3id.org/ontometal")
+
+        graph.remove((ontology_uri, OWL.versionIRI, None))
+        graph.remove((ontology_uri, OWL.versionInfo, None))
+
         graph.add(
             (
                 ontology_uri,
                 OWL.versionIRI,
-                Literal(ONTOMETAL_VERSION, datatype=XSD.string),
+                Literal(f"{ontology_uri}-{ONTOMETAL_VERSION}"),
             )
         )
         graph.add(
             (
                 ontology_uri,
                 OWL.versionInfo,
-                Literal(ONTOMETAL_VERSION_INFO, datatype=XSD.string),
+                Literal(f"{ONTOMETAL_VERSION_INFO}"),
             )
         )
 
